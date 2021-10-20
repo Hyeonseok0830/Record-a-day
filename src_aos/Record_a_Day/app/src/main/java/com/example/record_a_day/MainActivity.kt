b2681b3646928bdbcd3a003efa0ed864e42a4cfa
Hyeonseok <96hyeonseok@gmail.com>
2021-10-21 01:41:12 +0900

    프로젝트, 설계서 생성
    -logo 제작
    -aos 프로젝트 생성
    -설계서 작성

--- src_aos/Record_a_Day/app/src/main/java/com/example/record_a_day/MainActivity.kt
+++ src_aos/Record_a_Day/app/src/main/java/com/example/record_a_day/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.record_a_day
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.record_a_day.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding:ActivityMainBinding? = null
+
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+
+    override fun onDestroy() {
+
+        mBinding = null
+        super.onDestroy()
     }
 }
