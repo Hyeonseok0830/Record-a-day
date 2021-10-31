ed172c622764e16eb47e2e9bd41334deb6fc9cc9
Hyeonseok <96hyeonseok@gmail.com>
2021-11-01 01:21:01 +0900

    회원가입, 로그인 기능 구현
    - 회원가입 시 비밀번호 AES 암호화
    - 회원 정보 firestore 저장
    - 로그인 시 firestore 조회

---
+++ src_aos/Record_a_Day/app/src/main/java/com/example/record_a_day/data/UserData.kt
@@ -0,0 +1,17 @@
+package com.example.record_a_day.data
+
+class UserData{
+    var name:String = ""
+    var id:String =""
+    var pw:String =""
+    var birth:String =""
+    var phone:String =""
+    constructor(name:String,id:String,pw:String,birth:String,phone:String){
+        this.name = name
+        this.id = id
+        this.pw = pw
+        this.birth = birth
+        this.phone = phone
+    }
+}
+
