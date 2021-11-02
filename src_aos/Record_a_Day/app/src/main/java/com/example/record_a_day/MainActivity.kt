0895b5e1a254a88e25ea5b52cd8f186fc9eb2578
Hyeonseok <96hyeonseok@gmail.com>
2021-11-03 00:23:57 +0900

    메인 화면 하단 버튼 추가
    하단 메뉴 버튼 선택 시 세부 버튼 5개 보이기 처리
    3초 뒤 자동으로 숨김 처리

--- src_aos/Record_a_Day/app/src/main/java/com/example/record_a_day/MainActivity.kt
+++ src_aos/Record_a_Day/app/src/main/java/com/example/record_a_day/MainActivity.kt
@@ -2,7 +2,13 @@ package com.example.record_a_day
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.example.record_a_day.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +16,9 @@ class MainActivity : AppCompatActivity() {
 
     private val binding get() = mBinding!!
 
+    private var show = false
+
+    private val TAG = "TESTTEST"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,7 +29,34 @@ class MainActivity : AppCompatActivity() {
 
     }
     fun init(){
-
+        //하단 메뉴 버튼 클릭
+        binding.moreBtn.setOnClickListener {
+            if(!show)
+                showMoreBtn()
+        }
+    }
+    fun showMoreBtn(){
+        show=true
+        binding.myinfoBtn.visibility = View.VISIBLE
+        binding.recordBtn.visibility = View.VISIBLE
+        binding.taskListBtn.visibility = View.VISIBLE
+        binding.goalBtn.visibility = View.VISIBLE
+        binding.shareBtn.visibility = View.VISIBLE
+        CoroutineScope(Main).launch {
+            delay(3000L)
+            Log.d(TAG, "showMoreBtn: $show")
+            hideMoreBtn()
+        }
+    }
+    fun hideMoreBtn(){
+        if(show) {
+            binding.myinfoBtn.visibility = View.INVISIBLE
+            binding.recordBtn.visibility = View.INVISIBLE
+            binding.taskListBtn.visibility = View.INVISIBLE
+            binding.goalBtn.visibility = View.INVISIBLE
+            binding.shareBtn.visibility = View.INVISIBLE
+            show = false
+        }
     }
     override fun onDestroy() {
 
