b5197d5506e1d52322095f8a0ce34d11005a9014
Hyeonseok <96hyeonseok@gmail.com>
2022-01-15 17:13:26 +0900

    로그인 시 애니메이션 적용
    - 로그인 버튼 선택 시 LottieAnimation 적용

--- src_aos/Record_a_Day/app/src/main/java/com/example/record_a_day/LoginActivity.kt
+++ src_aos/Record_a_Day/app/src/main/java/com/example/record_a_day/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.record_a_day
 
+import android.animation.Animator
 import android.content.Intent
 import android.os.Bundle
 import android.text.SpannableString
@@ -9,6 +10,8 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.animation.keyframe.BaseKeyframeAnimation
 
 import com.example.record_a_day.databinding.ActivityLoginBinding
 import com.example.record_a_day.manager.PreferenceManager
@@ -38,12 +41,47 @@ class LoginActivity : AppCompatActivity() {
     //private val app_mode = "DEBUG" // 메인 화면 진입
     private val app_mode = "COMM"
 
+    private var animationView:LottieAnimationView ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Log.i(TAG, "onCreate: $this")
         mBinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        animationView = findViewById(R.id.animationView)
+        buttonInit()
+        with(animationView) {
+            this?.addAnimatorListener(object: Animator.AnimatorListener {
+                override fun onAnimationStart(animation: Animator?) {
+                    //TODO("Not yet implemented")
+                }
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    //TODO("Not yet implemented")
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {
+                    //TODO("Not yet implemented")
+                }
+
+                override fun onAnimationRepeat(animation: Animator?) {
+                    //TODO("Not yet implemented")
+                }
+
+            })
+        }
+
+
+    }
+
+    private fun buttonInit() {
         binding.loginBtn.setOnClickListener {
+            binding.animationView.visibility = View.VISIBLE
+            if(animationView!=null){
+                animationView!!.bringToFront()
+                binding.loginBtn.visibility=View.GONE
+                animationView!!.playAnimation()
+            }
             var id = binding.loginId.text.toString()
             if(app_mode.equals("DEBUG")){
                 if(binding.autoLogin.isChecked) {
@@ -55,7 +93,6 @@ class LoginActivity : AppCompatActivity() {
 
                 var intent = Intent(this@LoginActivity, MainActivity::class.java)
                 startActivity(intent)
-                finish()
             } else {
                 firestore.collection("user")
                     .whereEqualTo("id", binding.loginId.text.toString())
@@ -71,11 +108,19 @@ class LoginActivity : AppCompatActivity() {
                                     PreferenceManager.setString(this,AUTO_LOGIN_KEY,"")
                                 }
                                 PreferenceManager.setString(this, USER_INFO_KEY,"$name|$id")
-                                var intent = Intent(this@LoginActivity, MainActivity::class.java)
-                                startActivity(intent)
-                                finish()
+                                if(!binding.loginId.text.toString().isNullOrEmpty()) {
+                                    var intent = Intent(this@LoginActivity, MainActivity::class.java)
+                                    startActivity(intent)
+                                } else {
+                                    animationView!!.cancelAnimation()
+                                    animationView!!.visibility = View.GONE
+                                    binding.loginBtn.visibility=View.VISIBLE
+
+                                }
 
                             } else {
+                                animationView!!.cancelAnimation()
+
                                 Toast.makeText(
                                     this@LoginActivity,
                                     "비밀번호가 일치하지 않습니다.",
@@ -86,6 +131,8 @@ class LoginActivity : AppCompatActivity() {
                         }
                     }
                     .addOnFailureListener { exception ->
+                        animationView!!.cancelAnimation()
+
                         Log.w(TAG, "Error getting documents: ", exception)
                     }
             }
@@ -104,6 +151,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     override fun onStart() {
+        Log.i(TAG, "onStart: $this")
         var auto_login = PreferenceManager.getString(this,AUTO_LOGIN_KEY)
         if(auto_login.isNullOrEmpty()){
             binding.loginBackground.visibility = View.GONE
@@ -111,15 +159,32 @@ class LoginActivity : AppCompatActivity() {
         } else{
             var intent = Intent(this@LoginActivity, MainActivity::class.java)
             startActivity(intent)
-            finish()
         }
         super.onStart()
 
     }
 
+    override fun onResume() {
+        Log.i(TAG, "onResume: $this")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.i(TAG, "onPause: $this")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.i(TAG, "onStop: $this")
+        if(animationView!=null){
+            animationView!!.cancelAnimation()
+            animationView = null
+        }
+        super.onStop()
+    }
 
     override fun onDestroy() {
-
+        Log.i(TAG, "onDestroy: $this")
         mBinding = null
         super.onDestroy()
     }
@@ -140,12 +205,13 @@ class LoginActivity : AppCompatActivity() {
             backKeyPressedTime = System.currentTimeMillis()
             Toast.makeText(this,"뒤로 가기를 한 번 더 누르시면 종료합니다.",Toast.LENGTH_SHORT).show()
         } else {
-            AppFinish()
+            FinishApp()
         }
     }
-    fun AppFinish(){
+    fun FinishApp(){
         finish()
         System.exit(0)
 
     }
+
 }
